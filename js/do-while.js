0455c2ed9c71ad0f688fca2311319219a287782b
Paulo Srur <paulopepesrur@gmail.com>
2023-05-30 21:55:19 -0300

    bucle while y do while

---
+++ js/do-while.js
@@ -0,0 +1,18 @@
+// let edad= parseInt(prompt(`ingrese su edad`));
+
+// while(edad < 5 || edad>130){
+//     console.log(`la edad ingresada no es correcta`)
+//     edad = parseInt(prompt(`ingrese su edad`));
+// }
+// console.log(`puede ingresar`)
+
+
+//*bucle Do while
+
+let edad;
+
+do{
+    edad= parseInt(prompt(`ingrese su edad`));
+
+}while(edad < 5 || edad>130)
+console.log('puede ingresar')
--- js/while.js
+++ js/while.js
@@ -1,1 +1,11 @@
 //!WHILE Y LOOP NO SON METODOS PUNTUALES O ESPECIFICOS DE UN ARRAY, SON FORMAS PARA REPETIR CIERTA CANTIDAD DE ACCIONES UN NUMERO DE VECES.
+
+const numero= parseInt(prompt(`ingresar el numero que desea ser multiplicado`));
+
+let i =11;
+while(i<=10){
+    const multiplicacion = i*numero;
+
+    console.log(`resultado de ${i} x ${numero} = ${i*numero}`);
+    i++;
+}
